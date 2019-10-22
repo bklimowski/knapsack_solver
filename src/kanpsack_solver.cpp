8a45648969bfcb5c016bcd164775f23a029fa5e7
Klimowski Bartłomiej <bartomiej.klimowski@intrum.net>
2019-10-23 01:38:16 +0200

    sorting and basic menu

--- src/kanpsack_solver.cpp
+++ src/kanpsack_solver.cpp
@@ -2,14 +2,14 @@
 // Name        : kanpsack_solver.cpp
 // Author      : Bartlomiej Klimowski
 // Version     :
-// Copyright   : 
+// Copyright   :
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
 /*
  * Stuff to do:
- * 1. Generate values from normal distribution
- * 2. Sorting enumerated list
+ * 1. Generate values from normal distribution - done
+ * 2. Sorting enumerated list - done
  * 3. Drop one element at random
  * 4. Sum list elements
  * 5. Set operators
@@ -22,41 +22,99 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <unistd.h>
 
-//template <typename T>
-//std::vector<size_t> sort_indexes(const std::vector<T> &v) {
-//
-//  // initialize original index locations
-//  std::vector<size_t> idx(v.size());
-//  std::iota(idx.begin(), idx.end(), 0);
-//
-//  // sort indexes based on comparing values in v
-//  std::sort(idx.begin(), idx.end(),
-//       [&v](size_t i1, size_t i2) {return v[i1] < v[i2];});
-//
-//  return idx;
-//}
+template <typename T>
+std::vector<size_t> sort_indexes(const std::vector<T> &v) {
 
-template <typename distribution>
-std::vector<double> draw_from_dist(int n_samples, distribution dist){
+  // initialize original index locations
+  std::vector<size_t> idx(v.size());
+  std::iota(idx.begin(), idx.end(), 0);
 
-	std::vector<double> g1;
+  // sort indexes based on comparing values in v
+  std::sort(idx.begin(), idx.end(),
+       [&v](size_t i1, size_t i2) {return v[i1] < v[i2];});
 
-	std::random_device rd {};
-	std::mt19937 mt(rd());
+  return idx;
+}
+
+template <typename A>
+void print_vector(std::vector<A> v) {
+	for (A ii: v) {
+		std::cout << ii << " ";
+	}
+	std::cout << std::endl;
+}
+
+template <typename distribution, typename random_generator>
+std::vector<double> draw_from_dist(int n_samples, distribution dist, random_generator rg){
+
+	std::vector<double> draws;
+	draws.reserve(n_samples);
 
     for (int i = 1; i <= n_samples; i++)
-        g1.push_back(std::abs(dist(mt)));
+    	draws.push_back(fabs(dist(rg)));
 
-    return g1;
+    return draws;
+}
+
+void program_intro(){
+	std::string program_name {"KNAPSACK SOLVER"};
+	std::string version {"version 0.1"};
+	std::string author {"author: Bartlomiej Klimowski"};
+
+	for (auto ii: program_name) {
+		std::cout << ii << " ";
+		sleep(0.2);
+	}
+
+	for (auto ii: version) {
+		std::cout << ii;
+		sleep(0.2);
+	}
+
+	std::cout << std::endl;
+
+	for (auto ii: author) {
+		std::cout << ii;
+		sleep(0.2);
+	}
+
+	std::cout << std::endl;
 }
 
 int main() {
 
-	std::normal_distribution<double> norm_dist(0.1, 1.0);
-	std::vector<double> normal_draws = draw_from_dist(100, norm_dist);
+	program_intro();
 
-	std::cout << normal_draws[1] << std::endl;
+	int capacity { 10 };
 
+	std::random_device rd {};
+	std::mt19937 mt(rd());
+
+	std::normal_distribution<double> norm_dist1(1, 1.0);
+	std::vector<double> weigths = draw_from_dist(10, norm_dist1, mt);
+
+
+	print_vector(weigths);
+	auto sorted_indexes = sort_indexes(weigths);
+	std::reverse(sorted_indexes.begin(), sorted_indexes.end());
+	print_vector(sorted_indexes);
+
+	int total_weigth { 0 };
+	int n_top_items { 0 };
+
+	while(total_weigth < capacity) {
+		total_weigth += weigths[sorted_indexes[n_top_items]];
+		n_top_items++;
+		std::cout << n_top_items;
+	}
+
+	std::cout << std::endl;
+
+	for (int ii = 0; ii < n_top_items; ++ii) {
+		std::cout << sorted_indexes[ii] + 1 << " ";
+	}
 }
+
 
